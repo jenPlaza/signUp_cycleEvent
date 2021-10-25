0d7380e3144eca58fc5a0b564ad53bfa9d7941bc
Jennifer Plaza <jennyp.plaza@gmail.com>
2021-10-25 15:48:51 -0400

    added a br tag after select input

--- cycleRegistrationScript.js
+++ cycleRegistrationScript.js
@@ -64,7 +64,7 @@ function getRegistered() {
     registration +=' <option value="Male">Male</option>';
     registration +=' <option value="Female">Female</option>';
   registration +=' </select>';
-
+  registration +=' <br />';
       /* Email: */
   registration +='<label for="00N3f000000uZR4">Email:</label>';
   registration += '<input  id="00N3f000000uZR4" maxlength="80" name="00N3f000000uZR4" size="20" type="text" placeholder="Non - Military Email Preferred" />';
